519d66374c78b4359690b331e14288cb47a50827
seantubbs <seantubbs@hotmail.co.uk>
2020-03-24 17:44:17 +0000

    New date changes

--- js/script.js
+++ js/script.js
@@ -483,7 +483,7 @@
 	// 06.2 Countdown
 	//================================================================================
 	var theday = new Date();
-	theday = new Date(2020, 4, 30);
+	theday = new Date(2021, 5, 21);
 	$('.countdown').countdown({until: theday, format: 'DHMS'});
 	
 	
